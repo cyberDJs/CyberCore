c0915545f8d3a0eb5e8d759ba0a1da30f930b82d
horsedriver <jan@jankoci.cz>
2026-09-02 21:29:12 +0200

    WB-LR0003: export evaluation and acceptance contracts

--- src/cybercore/longrun/__init__.py
+++ src/cybercore/longrun/__init__.py
@@ -1,4 +1,14 @@
+from cybercore.longrun.acceptance import AcceleratedAcceptanceHarness, SimulatedClock
 from cybercore.longrun.engine import LongRunEngine, StepResult
+from cybercore.longrun.evaluation import EvaluationResult, evidence_digest
 from cybercore.longrun.manifest import LongRunManifest
 
-__all__ = ["LongRunEngine", "LongRunManifest", "StepResult"]
+__all__ = [
+    "AcceleratedAcceptanceHarness",
+    "EvaluationResult",
+    "LongRunEngine",
+    "LongRunManifest",
+    "SimulatedClock",
+    "StepResult",
+    "evidence_digest",
+]
